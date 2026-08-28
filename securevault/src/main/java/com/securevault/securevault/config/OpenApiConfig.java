5d36faea8b16c550d51e04f4e710e2aafcf251f3
rajawadhiya7-blip <rajawadhiya7@gmail.com>
2026-08-28 18:13:01 +0530

    feat: add JWT bearer auth to swagger

---
+++ securevault/src/main/java/com/securevault/securevault/config/OpenApiConfig.java
@@ -0,0 +1,19 @@
+package com.securevault.securevault.config;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@OpenAPIDefinition(
+        security = @io.swagger.v3.oas.annotations.security.SecurityRequirement(name = "bearerAuth")
+)
+@SecurityScheme(
+        name = "bearerAuth",
+        type = SecuritySchemeType.HTTP,
+        scheme = "bearer",
+        bearerFormat = "JWT"
+)
+public class OpenApiConfig {
+}
