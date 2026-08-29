329b35c56659cdf3b7b2bc88bd6e062ccf33d261
rajawadhiya7-blip <rajawadhiya7@gmail.com>
2026-08-29 19:38:54 +0530

    fix: add JWT security scheme to Swagger UI

--- securevault/src/main/java/com/securevault/securevault/config/OpenApiConfig.java
+++ securevault/src/main/java/com/securevault/securevault/config/OpenApiConfig.java
@@ -2,6 +2,9 @@ package com.securevault.securevault.config;
 
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.tags.Tag;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -22,6 +25,12 @@ public class OpenApiConfig {
                         new Tag().name("1. Authentication").description("Register and login endpoints"),
                         new Tag().name("2. Secrets").description("Secret management endpoints"),
                         new Tag().name("3. User").description("User profile endpoints")
-                ));
+                ))
+                .addSecurityItem(new SecurityRequirement().addList("Bearer Authentication"))
+                .components(new Components()
+                        .addSecuritySchemes("Bearer Authentication", new SecurityScheme()
+                                .type(SecurityScheme.Type.HTTP)
+                                .scheme("bearer")
+                                .bearerFormat("JWT")));
     }
 }
